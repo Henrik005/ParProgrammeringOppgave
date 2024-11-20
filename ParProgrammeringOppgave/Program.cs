5f767a6927673f6e3c55a3001f91aa9c824e0e0a
Henrik005 <henrik.mathias.brathen@gmail.com>
2024-11-20 14:46:30 +0100

    Add project files.

---
+++ ParProgrammeringOppgave/Program.cs
@@ -0,0 +1,25 @@
+﻿var input = Console.ReadLine();
+var input2 = Console.ReadLine();
+PrintText();
+void PrintText()
+{
+    
+    Console.WriteLine($"{input}");
+    if(input2 == "1")
+    {
+        ReverseText();
+    } 
+    
+}
+
+void ReverseText()
+{
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        Console.Write(input[input.Length - i - 1]);
+    }
+    
+}
+
+
